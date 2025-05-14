85ca8eb19ea235ca7f6eef79da234a9f9460711e
Xandeco <>
2025-05-14 03:11:25 -0300

    tentando deploy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  reactStrictMode: true,
+  basePath: '/gabriel-pastel',
 };
 
 export default nextConfig;
